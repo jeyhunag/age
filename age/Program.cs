15053b825c46b5dce0103fbb29d1cde256f8c436
*jeyhun <*jeyhunag@code.edu.az*>
2022-05-17 14:23:25 +0300

    crvwdv

---
+++ age/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val1 = 1;
+            int val2 = 100;
+            for (int i = val1; i <= val2; i++)
+            {
+                if (i%2!=0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            
+        }
+    }
+}
